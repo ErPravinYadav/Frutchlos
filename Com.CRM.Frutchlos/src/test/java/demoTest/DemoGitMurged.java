bff9d132493e5357dd027a5a007be93519591fac
ErPravinYadav <pravinyadav8463@gmail.com>
2022-10-20 13:03:02 +0530

    Branch TC_01 by engineer Pravin first commit

---
+++ Com.CRM.Frutchlos/src/test/java/demoTest/DemoGitMurged.java
@@ -0,0 +1,15 @@
+package demoTest;
+
+import org.testng.annotations.Test;
+
+public class DemoGitMurged {
+
+	
+	@Test
+	public void demogit()
+	{
+		System.out.println("Changes made in repository");
+		System.out.println("This is demo");
+		System.out.println("Changes made in working directory");
+	}
+}
