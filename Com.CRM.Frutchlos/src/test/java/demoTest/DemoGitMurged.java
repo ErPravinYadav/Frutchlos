945ffb11acb23687d45cf61c321510e639c6a1ff
ErPravinYadav <pravinyadav8463@gmail.com>
2022-10-25 16:41:38 +0530

    second commit by pravin

--- Com.CRM.Frutchlos/src/test/java/demoTest/DemoGitMurged.java
+++ Com.CRM.Frutchlos/src/test/java/demoTest/DemoGitMurged.java
@@ -1,5 +1,7 @@
 package demoTest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class DemoGitMurged {
@@ -8,6 +10,16 @@ public class DemoGitMurged {
 	@Test
 	public void demogit()
 	{
+		WebDriver driver= new ChromeDriver();  //launch the empty browser
+		System.out.println("Changes made in repository");
+		System.out.println("This is demo");
+		System.out.println("Changes made in working directory");
+	}
+	
+	@Test
+	public void demogit0()
+	{
+		WebDriver driver= new ChromeDriver();  //launch the empty browser
 		System.out.println("Changes made in repository");
 		System.out.println("This is demo");
 		System.out.println("Changes made in working directory");
